07fdd7101d3de7ff8c1222b6825e2528424061a9
Julia Álvarez <alvarezjulia1@yahoo.es>
2019-04-22 13:05:45 +0200

    resolved merge conflict

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config()
 
 const bodyParser = require('body-parser')
@@ -13,24 +12,6 @@ const path = require('path')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
-=======
-require('dotenv').config();
-
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-
-
->>>>>>> andre
 
 mongoose
     .connect('mongodb://localhost/oi', { useNewUrlParser: true })
@@ -45,8 +26,6 @@ const app_name = require('./package.json').name
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
 
 const app = express()
-
-
 
 // Middleware Setup
 app.use(logger('dev'))
@@ -81,32 +60,32 @@ hbs.registerHelper('ifUndefined', (value, options) => {
 // default value for title local
 // app.locals.title = 'Express - Generated with IronGenerator';
 
-
 // Enable authentication using session + passport
-app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
-app.use(flash());
-require('./passport')(app);
+app.use(
+    session({
+        secret: 'irongenerator',
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
+)
+app.use(flash())
+require('./passport')(app)
 
 const loginCheck = (req, res, next) => {
-  app.locals.loggedIn = req.isAuthenticated()
-  next();
+    app.locals.loggedIn = req.isAuthenticated()
+    next()
 }
 
 app.use(loginCheck)
-  
 
-const index = require('./routes/index');
-app.use('/', index);
+const index = require('./routes/index')
+app.use('/', index)
 
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
+const authRoutes = require('./routes/auth')
+app.use('/auth', authRoutes)
 
 const eventRoutes = require('./routes/yourevents')
 app.use('/', eventRoutes)
-      
-module.exports = app;
+
+module.exports = app
